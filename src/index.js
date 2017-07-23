ac484a6b1686488266ff9a3fe8bc280a944dd597
Ravi Shankar <ravi_shankar_v@yahoo.com>
2017-07-23 16:22:43 +0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const App = () => {
+    return ( 
+        <SearchBar />
+    )
+}
+
+ReactDOM.render( <App /> , document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const config = {
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: [/node_modules/],
+            use: [{
+                loader: 'babel-loader'
+            }]
+        }]
+    },
+}
+
+module.exports = config;
